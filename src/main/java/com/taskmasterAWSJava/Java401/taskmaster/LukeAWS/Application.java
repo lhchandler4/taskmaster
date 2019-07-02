40c83ef330972f7df4e848fdd7a1d16464b421bd
Luke Chandler <lhchandler4@gmail.com>
2019-07-02 10:58:15 -0700

    initial commit

---
+++ src/main/java/com/taskmasterAWSJava/Java401/taskmaster/LukeAWS/Application.java
@@ -0,0 +1,13 @@
+package com.taskmasterAWSJava.Java401.taskmaster.LukeAWS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
